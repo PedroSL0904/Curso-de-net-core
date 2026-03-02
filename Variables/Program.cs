8fc3315ea3c81a9879f27c602ac095c7e3c299a4
PedroSL0904 <pedrolucio.0904@gmail.com>
2026-03-01 19:08:51 -0600

    Cambio de nombre y avance con variables

---
+++ Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. Declaración de Variables
+string mensaje = "¡Bienvenido a C#!";
+int anio = 2026;
+
+// 2. Tipos numéricos con decimales
+double valorPi = 3.14159;
+
+// 'decimal' requiere el sufijo 'm'. Usado para DINERO (Alta precisión)
+decimal precioProducto = 19.99m;
+
+// 'float' requiere el sufijo 'f'. Usado para Gráficos/Videojuegos (Menos precisión)
+float coordenadas = 5.45f;
+
+// 3. Salida en consola (Moderno: Interpolación de Cadenas $)
+// En lugar de {0}, usamos el signo $ para poner las variables directo en el texto.
+Console.WriteLine($"{mensaje} - Año: {anio}");
+
+Console.WriteLine("--------------------------------"); // Separador visual
+Console.WriteLine($"Dato Científico (Double): {valorPi}");
+Console.WriteLine($"Precio (Decimal):         ${precioProducto}");
+Console.WriteLine($"Posición (Float):         {coordenadas}f");
