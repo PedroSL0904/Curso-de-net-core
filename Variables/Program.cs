f4535b3f20a0b0d795c784c1e7e330cd14734f54
PedroSL0904 <pedrolucio.0904@gmail.com>
2026-03-03 18:39:38 -0600

    Clase de variables

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -11,11 +11,17 @@ decimal precioProducto = 19.99m;
 // 'float' requiere el sufijo 'f'. Usado para Gráficos/Videojuegos (Menos precisión)
 float coordenadas = 5.45f;
 
-// 3. Salida en consola (Moderno: Interpolación de Cadenas $)
-// En lugar de {0}, usamos el signo $ para poner las variables directo en el texto.
-Console.WriteLine($"{mensaje} - Año: {anio}");
+// 'bool' representa un valor verdadero o falso
+bool esSoltero = true;
 
-Console.WriteLine("--------------------------------"); // Separador visual
-Console.WriteLine($"Dato Científico (Double): {valorPi}");
-Console.WriteLine($"Precio (Decimal):         ${precioProducto}");
-Console.WriteLine($"Posición (Float):         {coordenadas}f");
+// 'char' representa un solo carácter. Se define con comillas simples
+char letraInicial = 'P';
+
+// 3. Salida en consola 
+Console.WriteLine($"{mensaje} - Año: {anio}");
+Console.WriteLine("--------------------------------"); 
+Console.WriteLine($"Dato Científico (Double):  {valorPi}");
+Console.WriteLine($"Precio (Decimal):          ${precioProducto}");
+Console.WriteLine($"Posición (Float):          {coordenadas}f");
+Console.WriteLine($"Estado Civil (Bool):       {esSoltero}");
+Console.WriteLine($"Letra Inicial (Char):      '{letraInicial}'");
