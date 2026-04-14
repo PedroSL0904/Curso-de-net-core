f38d1f9ec62747fa4378891e95b573c6e9934f38
PedroSL0904 <pedrolucio.0904@gmail.com>
2026-04-13 22:05:26 -0600

    Clase de manipulación de cadenas

---
+++ ManipulacionDeCadenas/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("--- Manipulación de Cadenas ---");
+
+string name = "Pedro";
+string name2 = "Juan";
+
+name += name2; // Concatenación
+
+Console.WriteLine($"Nombre concatenado: {name}");
