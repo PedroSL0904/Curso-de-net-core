e3a834abf9851246dc993a8f07fdd8130c0a2864
PedroSL0904 <pedrolucio.0904@gmail.com>
2026-04-13 21:53:57 -0600

    Clase de ciclo while y do while

---
+++ WhileAndDoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("--- Ciclo while ---");
+int contador = 1;
+
+while (contador <= 6)
+{
+    Console.WriteLine($"Iteración while: {contador}");
+    contador++; 
+}
+
+Console.WriteLine("\n--- 2. Ciclo do-while ---");
+int intentos = 1;
+
+do
+{
+    Console.WriteLine($"Iteración do-while: {intentos}");
+    intentos++;
+} while (intentos <= 6);
+
+
+Console.WriteLine("\n--- 3. Ciclo while ---");
+bool sistemaActivo = true;
+int tiempo = 1;
+
+while (sistemaActivo)
+{
+    Console.WriteLine($"Monitoreando máquina... segundo {tiempo}");
+
+    if (tiempo == 3) 
+    {
+        Console.WriteLine("¡Falla detectada! Apagando monitoreo...");
+        sistemaActivo = false; 
+        // break;
+    }
+
+    tiempo++;
+}
